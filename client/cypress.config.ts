080aca6d1f40e4601c01b0acd14de6b3b2336760
Natalia Dushkina <natalia.dushkina@groupcdg.com>
2023-02-26 18:15:14 +0000

    reset db for login test

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "cypress";
 const { connect, disconnect } = require("./cypress/support/db");
+const ObjectId = require("mongodb").ObjectId;
 
 export default defineConfig({
   env: {
     baseUrl: "http://localhost:3000",
+    backendUrl: "http://localhost:8080",
     userEmail: "somebody@gmail.com",
     userPassword: "12345678!",
+    userPasswordEncrypted:
+      "$2a$12$d8Z0ydkZ8dxvGWgSJrKzs.nMiE3f8PWfCCtUHo7SBuVc4oO9a7ZTe",
   },
   numTestsKeptInMemory: 10,
   e2e: {
@@ -18,6 +22,26 @@ export default defineConfig({
           console.log("clear users");
           await users.deleteMany({});
           await users.dropIndexes();
+
+          await disconnect();
+
+          return null;
+        },
+        async createUser() {
+          const db = await connect();
+          const users = db.collection("users");
+
+          const user = {
+            _id: new ObjectId(),
+            username: "Somebody",
+            email: "somebody@gmail.com",
+            password:
+              "$2a$12$d8Z0ydkZ8dxvGWgSJrKzs.nMiE3f8PWfCCtUHo7SBuVc4oO9a7ZTe",
+            createdAt: new Date(),
+          };
+
+          console.log("create user");
+          await users.insertOne(user);
 
           await disconnect();
 
--- client/cypress/e2e/logged_in.cy.js
+++ client/cypress/e2e/logged_in.cy.js
@@ -2,16 +2,12 @@ describe("user logs in", () => {
   before(() => {
     cy.tests_cleanup();
     cy.task("createUser");
+    cy.simulate_login(Cypress.env("userEmail"), Cypress.env("userPassword"));
   });
 
   it("logged in user can see the protected page", () => {
-    cy.visit(Cypress.env("baseUrl"));
-    cy.get('[data-qa="protected-link"]').click();
-    cy.get('[data-qa="sign-in"]').should("be.visible");
+    cy.visit(`${Cypress.env("baseUrl")}/protected`);
 
-    cy.get('[data-qa="email"]').type(Cypress.env("userEmail"));
-    cy.get('[data-qa="password"]').type(Cypress.env("userPassword"));
-    cy.get('[data-qa="submit"]').click();
     cy.get('[data-qa="protected-page"]').should("be.visible");
     cy.get('[data-qa="sign-out-btn"]').should("be.visible");
 
